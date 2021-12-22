86998c9c7fedb5f0b8fd12bcafedfa562fa8bc81
Choi-Jiwon-38 <chlrhwldnjs@naver.com>
2021-12-23 03:21:44 +0900

    Create 유기농 배추.py(Recursion Error)

---
+++ BOJ/Python3/깊이 우선 탐색(DFS)/유기농 배추.py
@@ -0,0 +1,44 @@
+# 백준 알고리즘 저지 1012번
+# https://www.acmicpc.net/problem/1012
+
+
+import sys
+
+input = sys.stdin.readline
+
+def dfs(x, y): # dfs 함수 생성
+    if x <= -1 or x >= N or y <= -1 or y >= M:
+        return False
+    
+    if field[x][y] == 0:
+        # 현재 노드 방문 처리
+        field[x][y] = 1
+        # 상하좌우 재귀적으로 호출
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    else:
+        return False
+
+
+
+T = int(input()) # 테스트 케이스의 개수 입력
+
+for _ in range(T):
+    M, N, K = map(int, input().split()) # 가로, 세로, 배추 위치 입력
+
+    field = [[0]*M for i in range(N)] # 1로 초기화된 배추 텃밭 생성
+    for _ in range(K): # 배추가 있는 곳은 0으로 초기회
+        y, x = map(int, input().split())
+        field[x][y] = 0
+
+    result = 0
+    for i in range(N):
+        for j in range(M):
+            # 현재 위치에서 dfs 실행
+            if dfs(i, j) == True:
+                result += 1
+
+    print(result)
