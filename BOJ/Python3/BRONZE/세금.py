efdd7a8b0e4709be0e36639dadae70a563053199
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-03-10 14:26:54 +0900

    22.03.10

---
+++ BOJ/Python3/BRONZE/세금.py
@@ -0,0 +1,7 @@
+# 백준 온라인 저지 20492번
+
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+print(int(num * 0.78), int(num - (num * 0.2 * 0.22)))
---
+++ BOJ/Python3/BRONZE/큰 수 곱셈.py
@@ -0,0 +1,7 @@
+# 백준 13277번
+
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+print(A * B)
