c438035ac40875e7607755c6713edbb6cd2561c0
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2024-01-05 18:31:16 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 29.68 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for (int x : arr)
+    {
+        if (x >= 50 && x % 2 == 0)
+            answer.push_back(x / 2);
+        else if (x < 50 && x % 2 != 0)
+            answer.push_back(x * 2);
+        else
+            answer.push_back(x);
+    }
+    return answer;
+}
