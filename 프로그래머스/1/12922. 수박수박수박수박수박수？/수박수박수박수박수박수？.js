2aeede092ec431f75d341fe9648ad5e15b6d5896
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2024-04-04 01:08:04 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = '';
+    
+    if (n % 2 === 0) {
+        answer += "수박".repeat(n / 2);
+    } else {
+        answer += "수박".repeat((n - 1) / 2);
+        answer += "수"
+    }
+    
+    return answer;
+}
