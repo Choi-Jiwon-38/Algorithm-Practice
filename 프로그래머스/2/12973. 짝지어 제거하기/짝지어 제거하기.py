d6d0b227c49da1d3b4187977b54351aeaab8cc6d
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2024-12-24 17:31:06 +0900

    [level 2] Title: 짝지어 제거하기, Time: 142.21 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,17 +1,15 @@
-def solution(s):
-    a = ["" for _ in range(len(s) + 1)]
+from collections import deque
 
-    # '커서'라고 지칭
-    idx = 0
+def solution(s):
+    q = deque()
+    
     for c in s:
-        if a[idx] == '':    # 현재 커서에 비어있는 경우
-            a[idx] = c      # 다음에 올 값을 채워준다
-        elif (s[idx] == c): # 현재 커서와 다음에 올 값이 같은 경우
-            a[idx] = ''     # 지움 처리
-            idx -= 1
-        elif (s[idx] != c): # 현재 커서와 다음에 올 값이 다른 경우
-            idx += 1        
-            a[idx] = c      # 다음 칸에 채움 처리
+        if len(q) == 0:
+            q.append(c)
+        else:
+            if q[-1] == c:
+                q.pop()
+            else:
+                q.append(c)
             
-    # 전체를 짝지어 제거할 수 있는 문자열이었다면 a[idx]가 ''일 것
-    return 1 if a[idx] == '' else 0
+    return 1 if len(q) == 0 else 0
