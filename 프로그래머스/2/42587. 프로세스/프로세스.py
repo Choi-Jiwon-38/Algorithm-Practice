3c22d31427a2fe779c2ae0d5f71db3855c52589c
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2025-01-06 15:05:10 +0900

    [level 2] Title: 프로세스, Time: 0.25 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    
+    q = deque()
+    
+    for i in range(len(priorities)):
+        q.append([priorities[i], i])
+        
+    priority_list = sorted(list(q))
+    it = len(priority_list) - 1
+    
+    while True:
+        if q[0][0] < priority_list[it][0]:
+            tmp = q.popleft()
+            q.append(tmp)
+        else:
+            answer += 1
+            tmp = q.popleft()
+            if tmp[0] == priority_list[it][0]:
+                it -= 1
+            
+            if tmp[1] == location:
+                return answer
+    
