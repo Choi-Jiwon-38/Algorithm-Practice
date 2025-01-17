b19c4e6455b588be61c212e219e583da1f8f62d1
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2025-01-17 16:58:58 +0900

    [Silver III] Title: N과 M (7), Time: 2004 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split(" "))
+nums = sorted(list(map(int, input().rstrip().split(" "))))
+answer = []
+
+def backtrack(depth = 0):
+    if (depth == m):
+        print(*answer)
+        return
+    
+    for num in nums:
+        answer.append(num)
+        backtrack(depth + 1)
+        answer.pop()
+
+backtrack()
