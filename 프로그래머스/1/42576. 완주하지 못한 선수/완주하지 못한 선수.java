efb8cf3b060edf8d7bca9ddc3626b213dfe1f54d
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2026-07-24 20:50:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.26 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,32 +1,6 @@
-import java.util.HashMap;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> players = new HashMap<>();
-        
-        for (String p: participant) {
-            if (players.containsKey(p)) {
-                players.put(p, players.get(p) + 1);
-            } else {
-                players.put(p, 1);
-            }
-        }
-        
-        for (String c: completion) {
-            players.put(c, players.get(c) - 1);
-            if (players.get(c) == 0) {
-                players.remove(c);
-            }
-        }
-        
-        System.out.println(players.keySet());
-        
-        
         String answer = "";
-        for (String key: players.keySet()) {
-            answer = key;
-        }
-        
         return answer;
     }
 }
