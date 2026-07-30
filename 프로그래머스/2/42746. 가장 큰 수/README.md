ad978001ef70c3dee5231a2f3e2f81bd05dc7f45
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2026-07-30 23:11:01 +0900

    [level 2] Title: 가장 큰 수, Time: 648.42 ms, Memory: 20.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 24.3 MB, 시간: 588.27 ms
+메모리: 20.2 MB, 시간: 648.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 00:06:05
+2026년 07월 30일 23:10:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,12 +1,13 @@
 from functools import cmp_to_key
 
-def compare(x, y):
-    return int(y + x) - int(x + y)
+def compare(n1, n2):
+    return int(str(n1) + str(n2)) - int(str(n2) + str(n1))
 
 def solution(numbers):
-    numbers = list(map(str, numbers))
     numbers.sort(key=cmp_to_key(compare))
-            
-    answer = ''.join(numbers)
+    answer = ''
+    
+    for n in numbers:
+        answer = str(n) + answer
     
     return '0' if answer[0] == '0' else answer
