81a1c838f22940779df7c00eed7e2181dd094155
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2026-07-31 15:24:04 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,21 +1,19 @@
-def dfs(visit, computers, index, answer):
-    if visit[index] == True:
-        return;
-    
-    visit[index] = True
-    
-    for i in range(len(computers)):
-        if computers[index][i] == 1:
-            dfs(visit, computers, i, answer)
-    
-
 def solution(n, computers):
     answer = 0
-    visit = [False for _ in range(len(computers))]
+    visited = [False for _ in range(n)]
 
-    for i in range(len(computers)):
-        if visit[i] == False:
+    def dfs(index):
+        nonlocal visited
+        visited[index] = True     
+        
+        for i in range(n):
+            if computers[index][i] == 1 and not visited[i]:
+                visited[i] = True
+                dfs(i)
+    
+    for i in range(n):
+        if not visited[i]:
             answer += 1
-            dfs(visit, computers, i, answer)
-
+            dfs(i)
+        
     return answer
