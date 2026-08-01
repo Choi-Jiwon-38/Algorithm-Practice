25ecaae50badd86a4ff4485878bc0f35ae269d25
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2026-08-01 19:53:22 +0900

    [level 2] Title: 카펫, Time: 62.47 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,8 @@
 def solution(brown, yellow):
-    for x in range(1, yellow + 1):
-        if yellow % x == 0:
-            y = yellow / x
-        
-            if brown == 2 * (x + y) + 4:
-                return [x + 2, y + 2] if x >= y else [y + 2, x + 2]
+    # x * y = brown + yellow
+    # 2 * x + 2 * y - 4 = brown
+
+    for x in range(3, 2000):
+        for y in range(1, x + 1):
+            if x * y == brown + yellow and 2 * x + 2 * y - 4 == brown:
+                return [x, y]
