e127c080ce5fe434551452ddf43c70d8c2c46878
Jiwon Choi <81795729+Choi-Jiwon-38@users.noreply.github.com>
2026-08-04 12:52:37 +0900

    [level 2] Title: 구명보트, Time: 7.62 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 9.06 ms
+메모리: 11.9 MB, 시간: 7.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 26일 15:54:40
+2026년 08월 04일 12:52:29
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,19 @@
 def solution(people, limit):
-    # two pointer와 greedy 이용
     people.sort()
+    
+    l = len(people)
+    i = 0
+    j = l - 1
     
     answer = 0
     
-    i = 0
-    j = len(people) - 1
-    
     while i <= j:
-        if people[i] + people[j] <= limit: # 남은 인원 중 가장 가벼운 사람과 가장 무거운 사람이 같이 타는 경우
-            i += 1 # 가벼운 사람을 태움
-            j -= 1 # 무거운 사람을 태움
-            answer += 1 # 배 1척 소모
-        else: # 못타는 경우에는 무거운 사람때문에 타지 못한 것임
-            j -= 1 # 무거운 사람을 태움
-            answer += 1 # 배 1척 소모
-
+        if people[i] + people[j] > limit:
+            j -= 1
+            answer += 1
+        else:
+            answer += 1
+            i += 1
+            j -= 1
+        
     return answer
